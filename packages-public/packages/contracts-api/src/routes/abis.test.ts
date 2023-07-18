96e0fc05f881f4d66c72d307d94cc184a85de7ce
Oscar Baracos <oscar.baracos@gmail.com>
2023-07-18 16:23:08 +0400

    Fix duplicate procedure and misc issues (#673)

--- packages-public/packages/contracts-api/src/routes/abis.test.ts
+++ packages-public/packages/contracts-api/src/routes/abis.test.ts
@@ -1,9 +1,16 @@
-import { createOpenApiExpressMiddleware, generateOpenApiDocument } from "trpc-openapi";
+import {
+    createOpenApiExpressMiddleware,
+    generateOpenApiDocument,
+} from "trpc-openapi";
 import { abiFunctionWithZod } from "@owlprotocol/zod-sol";
-import { generateGETForAbiFunction, generatePOSTForAbiFunction } from "./abis.js";
+import {
+    generateGETForAbiFunction,
+    generatePOSTForAbiFunction,
+} from "./abis.js";
 import { t } from "../trpc.js";
+import { OpenAPIV3 } from "openapi-types";
 
-describe("abiRouteTest", function () {
+describe("abiRouteTest", function() {
     const fnAbis = {
         empty: {
             inputs: [],
@@ -107,39 +114,58 @@ describe("abiRouteTest", function () {
     } as const;
 
     it("empty", () => {
-        const proc = generateGETForAbiFunction("MyFunction", abiFunctionWithZod(fnAbis.empty));
+        const proc = generateGETForAbiFunction(
+            "MyFunction",
+            abiFunctionWithZod(fnAbis.empty)
+        );
         const router = t.router({ proc });
         const middleware = createOpenApiExpressMiddleware({ router });
     });
 
     it("uint256", () => {
-        const proc = generateGETForAbiFunction("MyFunction", abiFunctionWithZod(fnAbis.uint256));
+        const proc = generateGETForAbiFunction(
+            "MyFunction",
+            abiFunctionWithZod(fnAbis.uint256)
+        );
         const router = t.router({ proc });
         const middleware = createOpenApiExpressMiddleware({ router });
-        const openApiDocument: OpenAPIV3.Document = generateOpenApiDocument(router, {
-            title: "Owl Contract Api",
-            description: "Specification for our API focused on contract interactions",
-            version: "0.0.1",
-            baseUrl: "http://localhost:3000/api",
-            docsUrl: "https://dev.owlprotocol.xyz",
-            tags: ["contract", "deployment"],
-        });
+        const openApiDocument: OpenAPIV3.Document = generateOpenApiDocument(
+            router,
+            {
+                title: "Owl Contract Api",
+                description:
+                    "Specification for our API focused on contract interactions",
+                version: "0.0.1",
+                baseUrl: "http://localhost:3000/api",
+                docsUrl: "https://dev.owlprotocol.xyz",
+                tags: ["contract", "deployment"],
+            }
+        );
     });
 
     it("non-tuple", () => {
-        const proc = generateGETForAbiFunction("MyFunction", abiFunctionWithZod(fnAbis.nonTuple));
+        const proc = generateGETForAbiFunction(
+            "MyFunction",
+            abiFunctionWithZod(fnAbis.nonTuple)
+        );
         const router = t.router({ proc });
         const middleware = createOpenApiExpressMiddleware({ router });
     });
 
     it.skip("non-tuple array", () => {
-        const proc = generateGETForAbiFunction("MyFunction", abiFunctionWithZod(fnAbis.nonTupleArray));
+        const proc = generateGETForAbiFunction(
+            "MyFunction",
+            abiFunctionWithZod(fnAbis.nonTupleArray)
+        );
         const router = t.router({ proc });
         const middleware = createOpenApiExpressMiddleware({ router });
     });
 
     it("non-tuple array, POST", () => {
-        const proc = generatePOSTForAbiFunction("MyFunction", abiFunctionWithZod(fnAbis.nonTupleArray));
+        const proc = generatePOSTForAbiFunction(
+            "MyFunction",
+            abiFunctionWithZod(fnAbis.nonTupleArray)
+        );
         const router = t.router({ proc });
         const middleware = createOpenApiExpressMiddleware({ router });
     });
--- packages-public/packages/contracts-api/src/routes/deployments.test.ts
+++ packages-public/packages/contracts-api/src/routes/deployments.test.ts
@@ -1,56 +1,56 @@
 import { Collection } from "@owlprotocol/contracts-sdk";
 import { appRouter } from "../router";
 
-async function deployERC721() {
-    const router = appRouter.createCaller({});
-    let contractAddress: string;
-    try {
-        const deployResult = await router.collection.postCollection({
-            symbol: "MY",
-            name: "My collection",
-            networkId: "80001",
-            admin: "0x434C7df2f06D6CD172a28cb71e2AFE6E1b974DBC",
-            contractType: "ERC721AutoId" as Collection.NFTContractType,
-            dnaType: "NONE" as Collection.DNAType,
-            dnaSchemaUrl:
-                "https://leovigna.mypinata.cloud/ipfs/QmbUcD2MRhHYVwEw3YEX3izMzVvZfT49CGfLhqdVRVcnZd",
-        });
-        contractAddress = deployResult.contractAddress;
-    } catch (e) {
-        console.log(e);
-        throw e;
-    }
-
-    if (!contractAddress) {
-        console.log("No contract address");
-        return;
-    }
-
-    try {
-        const mintResult = await router.abi.ERC721MintableAutoId.safeMint({
-            networkId: "80001",
-            address: contractAddress,
-            contractParameters: {
-                to: "0x434C7df2f06D6CD172a28cb71e2AFE6E1b974DBC",
-            },
-        });
-        console.log(mintResult.result);
-    } catch (e) {
-        console.log(e);
-        throw e;
-    }
-
-    try {
-        const result = await router.abi.ERC721MintableAutoId.tokenURI({
-            networkId: "80001",
-            address: contractAddress,
-            contractParameters: {
-                tokenId: "1",
-            },
-        });
-        console.log(result.result);
-    } catch (e) {
-        console.log(e);
-        throw e;
-    }
-}
+// async function deployERC721() {
+//     const router = appRouter.createCaller({});
+//     let contractAddress: string;
+//     try {
+//         const deployResult = await router.collection.postCollection({
+//             symbol: "MY",
+//             name: "My collection",
+//             networkId: "80001",
+//             admin: "0x434C7df2f06D6CD172a28cb71e2AFE6E1b974DBC",
+//             contractType: "ERC721AutoId" as Collection.NFTContractType,
+//             dnaType: "NONE" as Collection.DNAType,
+//             dnaSchemaUrl:
+//                 "https://leovigna.mypinata.cloud/ipfs/QmbUcD2MRhHYVwEw3YEX3izMzVvZfT49CGfLhqdVRVcnZd",
+//         });
+//         contractAddress = deployResult.contractAddress;
+//     } catch (e) {
+//         console.log(e);
+//         throw e;
+//     }
+//
+//     if (!contractAddress) {
+//         console.log("No contract address");
+//         return;
+//     }
+//
+//     try {
+//         const mintResult = await router.abi.ERC721MintableAutoId.safeMint({
+//             networkId: "80001",
+//             address: contractAddress,
+//             contractParameters: {
+//                 to: "0x434C7df2f06D6CD172a28cb71e2AFE6E1b974DBC",
+//             },
+//         });
+//         console.log(mintResult.result);
+//     } catch (e) {
+//         console.log(e);
+//         throw e;
+//     }
+//
+//     try {
+//         const result = await router.abi.ERC721MintableAutoId.tokenURI({
+//             networkId: "80001",
+//             address: contractAddress,
+//             contractParameters: {
+//                 tokenId: "1",
+//             },
+//         });
+//         console.log(result.result);
+//     } catch (e) {
+//         console.log(e);
+//         throw e;
+//     }
+// }
