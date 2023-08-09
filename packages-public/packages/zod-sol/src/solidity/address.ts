d1a6de8775a4bcc370bece38579b6f91c8ffc3c3
Leo Vigna <leo.vigna@gmail.com>
2023-08-09 12:04:35 +0300

    DEV-39-For-non-gelato-networks-add-EOA-funding-topup (#690)
    * add user route
    
    * refactor readme webhook, refactor barrel files
    
    * refactor firestore, implement topup function
    
    * refactor use networkId middleware
    
    * add topup funding for native & erc20 tokens

--- packages-public/packages/zod-sol/src/solidity/address.ts
+++ packages-public/packages/zod-sol/src/solidity/address.ts
@@ -1,7 +1,9 @@
+import { ethers } from "ethers";
 import { z } from "zod";
 
 export const addressRegex = /0x([a-fA-F0-9]){40}/;
 export const addressZod = z
     .string()
     .regex(addressRegex)
+    .transform((a) => ethers.utils.getAddress(a))
     .describe("An ethereum address");
