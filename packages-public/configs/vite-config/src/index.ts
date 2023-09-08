2f5589c8bcec1441e80c710f856d291e66661259
Leo Vigna <leo.vigna@gmail.com>
2023-09-08 17:00:31 +0400

    DEV-180-No-Code-Dashboard-Setup (#757)
    * update starter react
    
    * bump to react 18
    
    * add chakra
    
    * add tanstack router
    
    * refactor routes, update readme
    
    * fix routes, add redirect index
    
    * remove lintstated broken
    
    * fix lint files glob
    
    * fix tsconfig
    
    * add dashboard starter
    
    * filter scope of testing

--- packages-public/configs/vite-config/src/index.ts
+++ packages-public/configs/vite-config/src/index.ts
@@ -1,35 +1,35 @@
-import { defineConfig, Plugin, UserConfig } from 'vite';
+import { defineConfig, Plugin, UserConfig } from "vite";
 
 //ESBuild Plugins
 //https://medium.com/@ftaioli/using-node-js-builtin-modules-with-vite-6194737c2cd2
-import { NodeGlobalsPolyfillPlugin as esbuildGlobals } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin as esbuildPolyfills } from '@esbuild-plugins/node-modules-polyfill';
+import { NodeGlobalsPolyfillPlugin as esbuildGlobals } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin as esbuildPolyfills } from "@esbuild-plugins/node-modules-polyfill";
 
 //Rollup Plugins
-import rollupPolyfills from 'rollup-plugin-node-polyfills'
-import rollupInject from '@rollup/plugin-inject';
+import rollupPolyfills from "rollup-plugin-node-polyfills";
+import rollupInject from "@rollup/plugin-inject";
 
 //Vite Plugins
-import ReactPlugin from '@vitejs/plugin-react';
-import CheckerPlugin from 'vite-plugin-checker';
-import SVGRPlugin from 'vite-plugin-svgr';
-import DTSPlugin from 'vite-dts';
-import { nodePolyfills as vitePolyfills } from 'vite-plugin-node-polyfills'
+import ReactPlugin from "@vitejs/plugin-react-swc";
+import CheckerPlugin from "vite-plugin-checker";
+import SVGRPlugin from "vite-plugin-svgr";
+import DTSPlugin from "vite-plugin-dts";
+import { nodePolyfills as vitePolyfills } from "vite-plugin-node-polyfills";
 
-const NODE_ENV = process.env.NODE_ENV ?? 'development' /// process.env.MODE
+//const NODE_ENV = process.env.NODE_ENV ?? "development"; /// process.env.MODE
 export const define = {
     //patch ipfs utils
-    'globalThis.process.env.NODE_ENV': JSON.stringify(NODE_ENV),
+    //"globalThis.process.env.NODE_ENV": JSON.stringify(NODE_ENV),
     //pathc @storybook/theming
-    'process.env.FORCE_SIMILAR_INSTEAD_OF_MAP': JSON.stringify(false)
-}
+    //"process.env.FORCE_SIMILAR_INSTEAD_OF_MAP": JSON.stringify(false),
+};
 
 export const plugins = [
     vitePolyfills(),
     ReactPlugin(),
     SVGRPlugin({
         svgrOptions: {
-            icon: '100%',
+            icon: "100%",
         },
     }),
     DTSPlugin(),
@@ -41,48 +41,49 @@ export const plugins = [
             lintCommand: 'eslint .  --ext .ts,.tsx',
         },
         */
-    })
-] as Plugin[]
+    }),
+] as Plugin[];
 
 export const esbuildPlugins = [
     esbuildGlobals({
         process: true,
         buffer: true,
     }),
-    esbuildPolyfills()
-] as any[]
+    esbuildPolyfills(),
+] as any[];
 
 export const rollupPlugins = [
     rollupPolyfills(),
     rollupInject({
-        Buffer: ['buffer', 'Buffer'],
-    })
-] as any[]
+        Buffer: ["buffer", "Buffer"],
+    }),
+] as any[];
 
 export const alias = {
-    buffer: 'rollup-plugin-node-polyfills/polyfills/buffer-es6',
-    events: 'rollup-plugin-node-polyfills/polyfills/events',
-    http: 'rollup-plugin-node-polyfills/polyfills/http',
-    https: 'rollup-plugin-node-polyfills/polyfills/http',
-    process: 'rollup-plugin-node-polyfills/polyfills/process-es6',
-    stream: 'rollup-plugin-node-polyfills/polyfills/stream',
-    util: 'rollup-plugin-node-polyfills/polyfills/util',
-    querystring: 'rollup-plugin-node-polyfills/polyfills/qs',
-    url: 'rollup-plugin-node-polyfills/polyfills/url',
-    path: 'rollup-plugin-node-polyfills/polyfills/path',
-    'string_decoder/': 'rollup-plugin-node-polyfills/polyfills/string-decoder',
-    string_decoder: 'rollup-plugin-node-polyfills/polyfills/string-decoder',
-    punycode: 'rollup-plugin-node-polyfills/polyfills/punycode',
-}
+    buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
+    events: "rollup-plugin-node-polyfills/polyfills/events",
+    http: "rollup-plugin-node-polyfills/polyfills/http",
+    https: "rollup-plugin-node-polyfills/polyfills/http",
+    process: "rollup-plugin-node-polyfills/polyfills/process-es6",
+    stream: "rollup-plugin-node-polyfills/polyfills/stream",
+    util: "rollup-plugin-node-polyfills/polyfills/util",
+    querystring: "rollup-plugin-node-polyfills/polyfills/qs",
+    url: "rollup-plugin-node-polyfills/polyfills/url",
+    path: "rollup-plugin-node-polyfills/polyfills/path",
+    "string_decoder/": "rollup-plugin-node-polyfills/polyfills/string-decoder",
+    string_decoder: "rollup-plugin-node-polyfills/polyfills/string-decoder",
+    punycode: "rollup-plugin-node-polyfills/polyfills/punycode",
+};
 
+//https://sambitsahoo.com/blog/vite-code-splitting-that-works.html
 export const manualChunks = (id) => {
     const vendors = [
-        { match: /web3@/, chunk: 'web3' },
-        { match: /@web3-react/, chunk: 'web3-react' },
-        { match: /@walletconnect/, chunk: 'walletconnect' },
-        { match: /@fortawesome/, chunk: '@fortawesome' },
-        { match: /lodash@/, chunck: 'lodash' },
-        { match: /ipfs-http-client@/, chunk: 'ipfs-http-client' },
+        { match: /web3@/, chunk: "web3" },
+        { match: /@web3-react/, chunk: "web3-react" },
+        { match: /@walletconnect/, chunk: "walletconnect" },
+        { match: /@fortawesome/, chunk: "@fortawesome" },
+        { match: /lodash@/, chunck: "lodash" },
+        { match: /ipfs-http-client@/, chunk: "ipfs-http-client" },
         /*
         { match: /react@/, chunk: 'react' },
         { match: /react-dom@/, chunk: 'react-dom' },
@@ -90,34 +91,42 @@ export const manualChunks = (id) => {
         { match: /@chakra-ui/, chunk: 'chakra-ui' },
         { match: /@storybook/, chunk: 'storybook' },
         */
-    ]
-    for (let v of vendors) {
+    ];
+    for (const v of vendors) {
         if (id.match(v.match)) return v.chunk;
     }
 
-    if (id.includes('node_modules')) {
-        return 'vendor';
+    if (id.includes("node_modules")) {
+        return "vendor";
     }
-}
+};
 
 export const config = defineConfig({
     define,
     plugins,
     resolve: {
-        alias
+        alias,
     },
     optimizeDeps: {
         esbuildOptions: {
-            plugins: []//esbuildPlugins
-        }
+            plugins: [], //esbuildPlugins
+        },
     },
     build: {
         rollupOptions: {
-            plugins: [],//rollupPlugins,
+            plugins: [], //rollupPlugins,
             output: {
                 manualChunks,
-                inlineDynamicImports: false
-            }
-        }
-    }
+                inlineDynamicImports: false,
+            },
+        },
+    },
+    //@ts-expect-error
+    test: {
+        globals: false,
+        testTimeout: 20000,
+        threads: false,
+        watch: true,
+        include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
+    },
 }) as UserConfig;
