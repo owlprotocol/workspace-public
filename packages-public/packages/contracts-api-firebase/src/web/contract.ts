173034ac8d1652ee901245c11cd2faec71136254
Kevin El Sayed <124922925+tota79@users.noreply.github.com>
2023-09-20 14:52:20 +0400

    Contracts/firebase test (#794)
    * Fixed some styling
    
    * Layout fixed
    
    * promo code page
    
    * header
    
    * Changed payout to category
    
    * changed layout

--- packages-public/packages/contracts-api-firebase/src/web/contract.ts
+++ packages-public/packages/contracts-api-firebase/src/web/contract.ts
@@ -1,46 +1,40 @@
-import { DocumentData, doc, getDoc, getDocs, QueryDocumentSnapshot } from "firebase/firestore";
+import { doc, getDoc, getDocs } from "firebase/firestore";
 import { FirebaseError } from "firebase/app";
 import { contractsCol } from "./config.js";
 import { Contract } from "../models/Contract.js";
 
+function handleFirebaseError(e: FirebaseError) {
+    console.error(e);
+    console.log(e.code);
+    if (e.code === "permission-denied") {
+        throw new Error("User not allowed to access the data");
+    }
+    throw new Error(`Firebase error: ${e.message}`);
+}
+
 export async function getContract(id: string): Promise<Contract> {
     const contractRef = doc(contractsCol, id);
-    let contractSnapshot: DocumentData;
     try {
-        contractSnapshot = await getDoc(contractRef);
+        const contractSnapshot = await getDoc(contractRef);
+        if (!contractSnapshot.exists()) {
+            throw new Error("Contract not found");
+        }
+        return contractSnapshot.data() as Contract;
     } catch (e) {
         if (e instanceof FirebaseError) {
-            console.error(e);
-            const firebaseError = e as FirebaseError;
-            console.log(firebaseError.code);
-            if (firebaseError.code === "permission-denied") {
-                throw new Error("User not allowed to get document");
-            }
-            throw new Error(`Error getting document: ${firebaseError.message}`);
+            handleFirebaseError(e);
         }
         throw new Error(`Error getting document: ${e}`);
     }
-
-    if (!contractSnapshot.exists()) {
-        throw new Error("Contract not found");
-    }
-
-    return contractSnapshot.data();
 }
 
 export async function getAllContracts(): Promise<Contract[]> {
     try {
         const querySnapshot = await getDocs(contractsCol);
-        return querySnapshot.docs.map((doc: QueryDocumentSnapshot<DocumentData>) => doc.data() as Contract); //QueryDocumentSnapshot -> type representing document from firestore query, document data is placeholder for data
+        return querySnapshot.docs.map((doc) => doc.data() as Contract);
     } catch (e) {
         if (e instanceof FirebaseError) {
-            console.error(e);
-            const firebaseError = e as FirebaseError;
-            console.log(firebaseError.code);
-            if (firebaseError.code === "permission-denied") {
-                throw new Error("User not allowed to get documents");
-            }
-            throw new Error(`Error getting documents: ${firebaseError.message}`);
+            handleFirebaseError(e);
         }
         throw new Error(`Error getting documents: ${e}`);
     }
