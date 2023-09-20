ad01e0ff29792cb0fecd491ad055d0a8f83ac376
Anna <ann.kazannik@gmail.com>
2023-09-20 17:32:32 +0400

    Fix lint errors

--- packages-public/packages/contracts-api-firebase/src/models/CouponCampaign.ts
+++ packages-public/packages/contracts-api-firebase/src/models/CouponCampaign.ts
@@ -1,7 +1,7 @@
 export interface CouponCampaign {
-  readonly id: string;
-  readonly name: string;
-  readonly project: string;
-  readonly description?: string;
-  readonly status: "active" | "inactive";
+    readonly id: string;
+    readonly name: string;
+    readonly project: string;
+    readonly description?: string;
+    readonly status: "active" | "inactive";
 }
--- packages-public/packages/contracts-api-firebase/src/models/CouponInstance.ts
+++ packages-public/packages/contracts-api-firebase/src/models/CouponInstance.ts
@@ -1,14 +1,14 @@
 export interface CouponInstance {
-  readonly id: string;
-  readonly couponDefinition: string;
-  // The user that received the coupon
-  readonly owner: string;
-  readonly redeemed: boolean;
-  readonly createdAt: Date;
-  // Store where the coupon can be redeemed
-  readonly couponProject: string;
-  // The id of the Shopify order that generated the coupon
-  readonly shopifyOrderId: string;
-  // Store that executed the order
-  readonly orderProject: string;
+    readonly id: string;
+    readonly couponDefinition: string;
+    // The user that received the coupon
+    readonly owner: string;
+    readonly redeemed: boolean;
+    readonly createdAt: Date;
+    // Store where the coupon can be redeemed
+    readonly couponProject: string;
+    // The id of the Shopify order that generated the coupon
+    readonly shopifyOrderId: string;
+    // Store that executed the order
+    readonly orderProject: string;
 }
