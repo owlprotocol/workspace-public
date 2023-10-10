70fad8589d58bbd733c796f5339bbd27754ad812
Leo Vigna <leo.vigna@gmail.com>
2023-10-10 15:46:39 +0200

    fix dotenvconfig vercel

--- packages-public/configs/envvars/src/dotenvConfig.ts
+++ packages-public/configs/envvars/src/dotenvConfig.ts
@@ -10,17 +10,23 @@ export function dotenvConfig(nodeEnv = NODE_ENV, dotenvKey = DOTENV_KEY) {
     //Only works in Node Environment
     if (!isClient()) {
         if (dotenvKey) {
+            console.debug("Loading .env.vault ");
             //Load remote envvars
-            dotenv.config({ DOTENV_KEY: dotenvKey });
+            dotenv.config({ path: findConfig(".env.vault") ?? undefined, DOTENV_KEY: dotenvKey });
         } else if (nodeEnv === "development") {
+            console.debug("Loading .env");
             dotenv.config({ path: findConfig(".env") ?? undefined });
         } else if (nodeEnv === "test") {
+            console.debug("Loading .env");
             dotenv.config({ path: findConfig(".env") ?? undefined });
         } else if (nodeEnv === "ci") {
+            console.debug("Loading .env.ci");
             dotenv.config({ path: findConfig(".env.ci") ?? undefined });
         } else if (nodeEnv === "staging") {
             dotenv.config({ path: findConfig(".env.staging") ?? undefined });
+            console.debug("Loading .env.staging");
         } else if (nodeEnv === "production") {
+            console.debug("Loading .env.production");
             dotenv.config({ path: findConfig(".env.production") ?? undefined });
         }
     }
