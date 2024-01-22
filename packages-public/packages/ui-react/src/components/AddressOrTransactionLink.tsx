082705e0303e1d70199598b931bda28dd9b62b6b
Anna Kazannik <46112675+annkaz@users.noreply.github.com>
2024-01-22 12:20:51 +0400

    Add link to dashboard contracts page from the collection page (#1234)
    * Add link to dashboard contracts page from the collection page
    
    * Have each item in the address column point to the corresponding contract page in the dashboard
    
    * Have each item in the address column point to the corresponding contract page in the dashboard
    
    * Make each item in the address column point to the corresponding contract page in the dashboard

--- packages-public/packages/ui-react/src/components/AddressOrTransactionLink.tsx
+++ packages-public/packages/ui-react/src/components/AddressOrTransactionLink.tsx
@@ -4,13 +4,13 @@ import { truncateAddress } from "../utils/truncateAddress.js";
 
 interface AddressOrTransactionLinkProps {
     networkExplorerUrl: string;
-    type: "tx" | "address";
+    type: "tx" | "address" | "contracts";
     addressOrHash: string;
 }
 
 export const AddressOrTransactionLink = forwardRef<HTMLElement, AddressOrTransactionLinkProps & BoxProps>(
     ({ addressOrHash, networkExplorerUrl, type, ...boxProps }, ref) => (
-        <Box ref={ref} {...boxProps}>
+        <Box ref={ref} {...boxProps} cursor="pointer">
             {networkExplorerUrl ? (
                 <Link href={`${networkExplorerUrl}/${type}/${addressOrHash}`} isExternal>
                     <Text maxWidth="300px" overflow="hidden" textOverflow="ellipsis" whiteSpace="nowrap">
@@ -18,9 +18,11 @@ export const AddressOrTransactionLink = forwardRef<HTMLElement, AddressOrTransac
                     </Text>
                 </Link>
             ) : (
-                <Text maxWidth="300px" overflow="hidden" textOverflow="ellipsis" whiteSpace="nowrap">
-                    {truncateAddress(addressOrHash)}
-                </Text>
+                <Link href={`/${type}/${addressOrHash}`} isExternal>
+                    <Text maxWidth="300px" overflow="hidden" textOverflow="ellipsis" whiteSpace="nowrap">
+                        {truncateAddress(addressOrHash)}
+                    </Text>
+                </Link>
             )}
         </Box>
     ),
