0978548002db88ec4dbc0f1d3995b024c2b2daea
Oscar Baracos <oscar.baracos@gmail.com>
2024-05-22 16:36:06 +0800

    Fix docs screenshot style (#1567)

--- packages-public/docs/components/screenshot/index.jsx
+++ packages-public/docs/components/screenshot/index.jsx
@@ -5,8 +5,8 @@ export function Screenshot({ src, alt, full }) {
   return (
     <div
       className={cn(
-        '-mb-4 mt-6 flex justify-center overflow-hidden rounded-xl border dark:border-zinc-800',
-        full ? 'bg-white' : 'bg-zinc-100'
+                '-mb-4 mt-6 flex justify-center overflow-hidden rounded-xl border dark:border-zinc-800',
+                // full ? 'bg-white' : 'bg-zinc-100'
       )}
     >
       <Image
