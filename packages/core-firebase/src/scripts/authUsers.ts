6ace6b1aea34707605d18c6dbcf9f976283d38ca
Oscar Baracos <oscar.baracos@gmail.com>
2024-07-03 16:24:05 +0800

    [WIP] auth users migration script

---
+++ packages/core-firebase/src/scripts/authUsers.ts
@@ -0,0 +1,13 @@
+import { getFirebaseApp } from "@owlprotocol/crud-firebase/admin";
+
+export async function migrateAuthUsers() {
+    const { auth } = getFirebaseApp();
+    const users = await auth.listUsers(10);
+    console.log(users);
+    // TODO: for all users listed, get their email from the users collection, and add the email to the auth users using `updateUser`
+    // const uid = "user_2VKdvYq9WgokilCPzkWWHHoWGAb";
+    // const update = await auth.updateUser(uid, { email: "leo@owlprotocol.xyz" });
+    // console.log(update);
+}
+
+await migrateAuthUsers();
