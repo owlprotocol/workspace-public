8597873a6a4f52d3323fc20de1c1f04a6566078a
Leo Vigna <leo.vigna@gmail.com>
2024-09-09 03:09:44 +0200

    add cryptoalgebra contracts, update zod gen, update artifacts export

--- packages/contracts-algebra-integral/src/codegen/runHardhatArtifactsExport.ts
+++ packages/contracts-algebra-integral/src/codegen/runHardhatArtifactsExport.ts
@@ -1,3 +1,10 @@
 import { hardhatArtifactsExport } from "@owlprotocol/viem-utils/codegen";
 
-hardhatArtifactsExport();
+hardhatArtifactsExport("./src/artifacts", "./cache", [
+    //EntryPoint Simulation Contracts
+    //TODO: hardhatArtifactsExport caching doesn't work anymore 'EntryPoint', 'IEntryPoint' getting recompiled
+    "artifacts/contracts/**/*.json",
+    //Cached official builds
+    "node_modules/@cryptoalgebra/integral-periphery/artifacts/contracts/interfaces/IQuoterV2.sol/IQuoterV2.json",
+    "node_modules/@cryptoalgebra/integral-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json",
+]);
