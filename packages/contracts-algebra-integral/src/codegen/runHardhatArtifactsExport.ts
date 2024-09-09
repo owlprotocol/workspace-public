aba9283f22fc1e61714815745631d9894879ab14
Leo Vigna <leo.vigna@gmail.com>
2024-09-09 03:13:51 +0200

    remove dummy contracts

--- packages/contracts-algebra-integral/src/codegen/runHardhatArtifactsExport.ts
+++ packages/contracts-algebra-integral/src/codegen/runHardhatArtifactsExport.ts
@@ -3,7 +3,7 @@ import { hardhatArtifactsExport } from "@owlprotocol/viem-utils/codegen";
 hardhatArtifactsExport("./src/artifacts", "./cache", [
     //EntryPoint Simulation Contracts
     //TODO: hardhatArtifactsExport caching doesn't work anymore 'EntryPoint', 'IEntryPoint' getting recompiled
-    "artifacts/contracts/**/*.json",
+    // "artifacts/contracts/**/*.json",
     //Cached official builds
     "node_modules/@cryptoalgebra/integral-periphery/artifacts/contracts/interfaces/IQuoterV2.sol/IQuoterV2.json",
     "node_modules/@cryptoalgebra/integral-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json",
