1416bf157716bd75b34021eef1af1f3dabe1ff97
Leo Vigna <leo.vigna@gmail.com>
2024-09-14 02:08:11 +0200

    remove hard-coded configs, use gas estimation for setup

--- packages/chains/src/chains/sepolia.ts
+++ packages/chains/src/chains/sepolia.ts
@@ -1,5 +1,4 @@
 import { sepolia as sepoliaViem } from "viem/chains";
-import { parseEther } from "viem";
 import { defineNetwork } from "../defineChain.js";
 
 export const sepolia = /*#__PURE__*/ defineNetwork({
@@ -15,9 +14,4 @@ export const sepolia = /*#__PURE__*/ defineNetwork({
     faucets: {
         default: { name: "nero", url: "https://www.app.testnet.nerochain.io/faucet" },
     },
-    //~500 USD / chain
-    minPaymasterBalance: parseEther("0.05"),
-    targetPaymasterBalance: parseEther("0.1"),
-    minRelayerBalance: parseEther("0.05"),
-    targetRelayerBalance: parseEther("0.1"),
 });
