166703a5076e77a2b11e8518946efed7601e6448
Leo Vigna <leo.vigna@gmail.com>
2024-10-02 19:09:30 +0400

    add contracts-hyperlane

---
+++ packages/contracts-hyperlane/src/scripts/localhost.ts
@@ -0,0 +1,136 @@
+//@ts-nocheck
+import { Chain, createPublicClient, createWalletClient, encodeFunctionData, http } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { localhost } from "viem/chains";
+import {
+    encodeCallRemoteWithOverrides,
+    getLocalInterchainAccount,
+    getRemoteInterchainAccount,
+    quoteGasPayment,
+} from "../InterchainAccountRouter.js";
+import { defaultIsm as defaultIsmAbi } from "../artifacts/IMailbox.js";
+import { domains } from "../artifacts/IRouter.js";
+
+// Constants for local setup
+const MAILBOX_ADDRESS_LOCALHOST = "0x12975173B87F7595EE45dFFb2Ab812ECE596Bf84";
+const INTERCHAIN_ACCOUNT_ROUTER_ADDRESS_LOCALHOST = "0x29a79095352a718B3D7Fe84E1F14E9F34A35598e";
+const ISM_ADDRESS_LOCALHOST = "0x05B4CB126885fb10464fdD12666FEb25E2563B76";
+
+const localMainChain = { ...localhost, id: 31337 };
+const localRemoteChain = {
+    ...localhost,
+    id: 31338,
+    rpcUrls: { default: { http: ["http://127.0.0.1:8546"] } },
+} as Chain;
+
+const localMainClient = createPublicClient({
+    chain: localMainChain,
+    transport: http(),
+});
+
+const localRemoteClient = createPublicClient({
+    chain: localRemoteChain,
+    transport: http(),
+});
+
+const localMainWalletClient = createWalletClient({
+    chain: localMainChain,
+    transport: http(),
+    //anvil
+    account: privateKeyToAccount("0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e"),
+});
+
+// Uncomment if you need to set initialize the router
+// import { pad } from "viem";
+// import { enrollRemoteRouterAndIsm as enrollRemoteRouterAndIsmAbi } from "../artifacts/InterchainAccountRouter.js";
+// const initializeMainRouter = async () => {
+//     const localMainRelayerWalletClient = createWalletClient({
+//         chain: localMainChain,
+//         transport: http(),
+//         //anvil
+//         account: privateKeyToAccount("0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6"),
+//     });
+//
+//     await localMainRelayerWalletClient.writeContract({
+//         address: INTERCHAIN_ACCOUNT_ROUTER_ADDRESS_LOCALHOST,
+//         abi: [enrollRemoteRouterAndIsmAbi],
+//         functionName: "enrollRemoteRouterAndIsm",
+//         args: [
+//             localRemoteChain.id,
+//             pad(INTERCHAIN_ACCOUNT_ROUTER_ADDRESS_LOCALHOST, { size: 32 }),
+//             pad(ISM_ADDRESS_LOCALHOST, { size: 32 }),
+//         ],
+//     });
+// };
+
+async function main() {
+    // Get the interchain account address from the main chain using its owner
+    const interchainAccountFromMain = await getRemoteInterchainAccount({
+        publicClient: localMainClient,
+        remoteRouter: INTERCHAIN_ACCOUNT_ROUTER_ADDRESS_LOCALHOST,
+        owner: localMainWalletClient.account.address,
+        remoteIsm: ISM_ADDRESS_LOCALHOST,
+        mainRouter: INTERCHAIN_ACCOUNT_ROUTER_ADDRESS_LOCALHOST,
+    });
+
+    // Get interchain account address from the remote chain using the owner & origin
+    const interchainAccountFromRemote = await getLocalInterchainAccount({
+        publicClient: localRemoteClient,
+        origin: localMainChain.id,
+        router: INTERCHAIN_ACCOUNT_ROUTER_ADDRESS_LOCALHOST,
+        owner: localMainWalletClient.account.address,
+        ism: ISM_ADDRESS_LOCALHOST,
+    });
+
+    // Uncomment if you need to set initialize the router
+    // await initializeMainRouter();
+
+    //should be the same
+    console.debug({ interchainAccountFromMain, interchainAccountFromRemote });
+
+    // get default ISM
+    const defaultIsm = await localRemoteClient.readContract({
+        address: MAILBOX_ADDRESS_LOCALHOST,
+        abi: [defaultIsmAbi],
+        functionName: "defaultIsm",
+    });
+
+    // we test out a simple read call from the interchain account
+    const calls = [
+        {
+            to: INTERCHAIN_ACCOUNT_ROUTER_ADDRESS_LOCALHOST,
+            data: encodeFunctionData({
+                abi: [domains],
+                functionName: "domains",
+            }),
+        } as const,
+    ];
+
+    // call remote data
+    const callRemoteData = encodeCallRemoteWithOverrides({
+        destination: localRemoteChain.id,
+        router: INTERCHAIN_ACCOUNT_ROUTER_ADDRESS_LOCALHOST,
+        ism: defaultIsm,
+        calls,
+    });
+
+    //execute
+    const hash = await localMainWalletClient.sendTransaction({
+        to: INTERCHAIN_ACCOUNT_ROUTER_ADDRESS_LOCALHOST,
+        data: callRemoteData,
+    });
+
+    console.debug({ hash });
+    const receipt = await localMainClient.waitForTransactionReceipt({ hash });
+    console.debug({ receipt });
+
+    const quote = await quoteGasPayment({
+        publicClient: localMainClient,
+        destination: localRemoteChain.id,
+        gasLimit: 100000000000000n,
+        router: INTERCHAIN_ACCOUNT_ROUTER_ADDRESS_LOCALHOST,
+    });
+    console.log({ quote });
+}
+
+main();
---
+++ packages/contracts-hyperlane/src/scripts/testnets.ts
@@ -0,0 +1,166 @@
+//@ts-nocheck
+import {
+    Address,
+    createPublicClient,
+    createWalletClient,
+    encodeFunctionData,
+    http,
+    parseEther,
+    parseEventLogs,
+} from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { bscTestnet, scrollSepolia } from "viem/chains";
+import { GithubRegistry } from "@hyperlane-xyz/registry";
+import {
+    encodeCallRemoteWithOverrides,
+    getLocalInterchainAccount,
+    getRemoteInterchainAccount,
+} from "../InterchainAccountRouter.js";
+import { defaultIsm as defaultIsmAbi, DispatchId as DispatchIdEvent } from "../artifacts/IMailbox.js";
+import { domains } from "../artifacts/IRouter.js";
+import "dotenv/config";
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+if (!PRIVATE_KEY) throw new Error("Need PRIVATE_KEY");
+
+interface InterchainAddresses {
+    mailbox: Address;
+    interchainAccountIsm: Address;
+    interchainAccountRouter: Address;
+}
+
+const bscTestnetClient = createPublicClient({
+    chain: bscTestnet,
+    transport: http(),
+});
+
+const scrollSepoliaClient = createPublicClient({
+    chain: scrollSepolia,
+    transport: http(),
+});
+
+const bscTestnetWalletClient = createWalletClient({
+    chain: bscTestnet,
+    transport: http(),
+    //anvil
+    account: privateKeyToAccount(PRIVATE_KEY as Address),
+});
+
+const PROXY_DEPLOYED_URL = "https://proxy.hyperlane.xyz";
+const registry = new GithubRegistry({
+    proxyUrl: PROXY_DEPLOYED_URL,
+});
+
+export async function getInterchainChainAddresses(chainName: string) {
+    const addresses = await registry.getChainAddresses(chainName);
+    if (!addresses) throw new Error(`${chainName} addresses not found`);
+
+    if (!addresses["mailbox"] || !addresses["interchainAccountIsm"] || !addresses["interchainAccountRouter"]) {
+        console.log({ addresses });
+        throw new Error(`${chainName} not supported`);
+    }
+
+    return addresses as any as InterchainAddresses;
+}
+
+async function main() {
+    const bscTestnetAddresses = await getInterchainChainAddresses("bsctestnet");
+    const scrollSepoliaAddresses = await getInterchainChainAddresses("scrollsepolia");
+
+    // Get the interchain account address from the main chain using its owner
+    const interchainAccountFromMain = await getRemoteInterchainAccount({
+        publicClient: bscTestnetClient,
+        remoteRouter: scrollSepoliaAddresses.interchainAccountRouter,
+        remoteIsm: scrollSepoliaAddresses.interchainAccountIsm,
+        mainRouter: bscTestnetAddresses.interchainAccountRouter,
+        owner: bscTestnetWalletClient.account.address,
+    });
+
+    // Get interchain account address from the remote chain using the owner & origin
+    const interchainAccountFromRemote = await getLocalInterchainAccount({
+        publicClient: scrollSepoliaClient,
+        origin: bscTestnet.id,
+        router: scrollSepoliaAddresses.interchainAccountRouter,
+        owner: bscTestnetWalletClient.account.address,
+        ism: scrollSepoliaAddresses.interchainAccountIsm,
+    });
+
+    //should be the same
+    console.debug({ interchainAccountFromMain, interchainAccountFromRemote });
+
+    // get default ISM
+    const defaultIsm = await scrollSepoliaClient.readContract({
+        address: scrollSepoliaAddresses.mailbox,
+        abi: [defaultIsmAbi],
+        functionName: "defaultIsm",
+    });
+
+    // we test out a simple read call from the interchain account
+    const calls = [
+        {
+            to: scrollSepoliaAddresses.interchainAccountRouter,
+            data: encodeFunctionData({
+                abi: [domains],
+                functionName: "domains",
+            }),
+        } as const,
+    ];
+
+    // call remote data
+    // const callRemoteData = encodeCallRemote({
+    //     destination: scrollSepolia.id,
+    //     calls,
+    //     hookMetadata: "0x",
+    // });
+
+    const callRemoteData = encodeCallRemoteWithOverrides({
+        destination: scrollSepolia.id,
+        router: scrollSepoliaAddresses.interchainAccountRouter,
+        ism: defaultIsm,
+        calls,
+    });
+
+    const remoteGasEstimate = await scrollSepoliaClient.estimateGas(calls[0]);
+
+    console.log({ remoteGasEstimate });
+
+    // const gasQuote = await quoteGasPayment({
+    //     publicClient: bscTestnetClient,
+    //     destination: scrollSepolia.id,
+    //     gasLimit: remoteGasEstimate,
+    //     router: bscTestnetAddresses.interchainAccountRouter,
+    // });
+    //
+    // console.log({ gasQuote });
+    //
+    // // 130%
+    // const gasQuoteOverestimate = (gasQuote * 13n) / 10n;
+    // console.log({gasQuoteOverestimate})
+
+    //execute
+    const hash = await bscTestnetWalletClient.sendTransaction({
+        to: bscTestnetAddresses.interchainAccountRouter,
+        data: callRemoteData,
+        value: parseEther("0.001"),
+    });
+
+    console.debug({ hash });
+    const receipt = await bscTestnetClient.waitForTransactionReceipt({ hash });
+    console.debug({ receipt });
+
+    // const remoteCallReceipt = await bscTestnetClient.waitForTransactionReceipt({
+    //     hash: "0x7341b7be338e459352bc2173c2842748abad58f9df2a65f201c155f9d44875c9",
+    // });
+    //
+    const remoteCallReceipt = receipt;
+
+    const dispatchIdEvent = parseEventLogs({
+        abi: [DispatchIdEvent],
+        logs: remoteCallReceipt.logs,
+        eventName: "DispatchId",
+    })[0];
+    const messageId = dispatchIdEvent.args.messageId;
+    console.log({ messageId });
+}
+
+main();
