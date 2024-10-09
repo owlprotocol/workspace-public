018db991053cde7ec4c7ab909c1a50e9dc520717
Oscar Baracos <oscar.baracos@gmail.com>
2024-10-09 11:46:05 +0400

    Fix Create2Factory

--- packages/contracts-create2factory/src/Create2Factory/constants.ts
+++ packages/contracts-create2factory/src/Create2Factory/constants.ts
@@ -4,7 +4,7 @@ import { parseGwei, Address } from "viem";
  * Create2Factory address when deployed using DeterministicDeployer
  * 0x62366409c9E4D9c7b255d6A8990320A6e4c29B17
  **/
-export const CREATE2_FACTORY_ADDRESS: Address = "0x62366409c9E4D9c7b255d6A8990320A6e4c29B17";
+export const CREATE2_FACTORY_ADDRESS: Address = "0x57318Dc30FE4da0a1b20eBbD4Dfd16aa66cfDB46";
 
 /** ETH transaction base gas cost */
 export const ETH_TX_BASE_GAS = 21000n;
