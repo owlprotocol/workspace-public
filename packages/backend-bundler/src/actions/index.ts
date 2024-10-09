db18bae607b7a75821ee75fb49ccad95d276ccf3
Leo Vigna <leo.vigna@gmail.com>
2024-10-09 13:25:56 +0400

    backend-bundler: remove old paymaster, bundler functions

---
+++ packages/backend-bundler/src/actions/index.ts
@@ -0,0 +1,2 @@
+export * from "./bundler/index.js";
+export * from "@owlprotocol/contracts-account-abstraction/actions";
--- packages/backend-bundler/src/index.ts
+++ packages/backend-bundler/src/index.ts
@@ -1,6 +1,2 @@
-export * from "./createBundlerBackend.js";
-export * from "./createPaymasterBackend.js";
-export * from "./createBundlerBackendWithFirebase.js";
-
-export * from "./isBundlerRpcMethod.js";
-export * from "./isPaymasterRpcMethod.js";
+export * from "./actions/index.js";
+export * from "./clients/index.js";
