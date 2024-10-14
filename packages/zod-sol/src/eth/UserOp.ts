d0fe5ac90e07882ad8607185f6cc90836b777c76
Leo Vigna <leo.vigna@gmail.com>
2024-10-14 14:59:17 +0400

    zod-sol: UseROp use viem model

--- packages/zod-sol/src/eth/UserOp.ts
+++ packages/zod-sol/src/eth/UserOp.ts
@@ -1,6 +1,7 @@
 /***** UserOp *****/
 
 import { z } from "zod";
+import { UserOperation } from "viem/account-abstraction";
 import { quantityDecodeZod, quantityEncodeZod } from "./math.js";
 import { addressZod } from "../solidity/address.js";
 import { bytesZod } from "../solidity/bytes.js";
@@ -9,67 +10,19 @@ import { bytesZod } from "../solidity/bytes.js";
  * UserOp with mixed types
  * Matches both encoded RPC & decoded TS types.
  */
-export interface UserOpInput {
-    sender: `0x${string}`;
-    nonce: `0x${string}` | number | bigint;
-    factory?: `0x${string}`;
-    factoryData?: `0x${string}`;
-    callData: `0x${string}`;
-    callGasLimit: `0x${string}` | number | bigint;
-    verificationGasLimit: `0x${string}` | number | bigint;
-    preVerificationGas: `0x${string}` | number | bigint;
-    maxFeePerGas: `0x${string}` | number | bigint;
-    maxPriorityFeePerGas: `0x${string}` | number | bigint;
-    paymaster?: `0x${string}`;
-    paymasterVerificationGasLimit?: `0x${string}` | number | bigint;
-    paymasterPostOpGasLimit?: `0x${string}` | number | bigint;
-    paymasterData?: `0x${string}`;
-    signature: `0x${string}`;
-}
+export type UserOpInput = UserOperation<"0.7", `0x${string}` | number | bigint>;
 
 /**
  * UserOp with encoded types
  * Bigint converted to Hex to support Firebase.
  */
-export interface UserOpEncoded {
-    sender: `0x${string}`;
-    nonce: `0x${string}`;
-    factory?: `0x${string}`;
-    factoryData?: `0x${string}`;
-    callData: `0x${string}`;
-    callGasLimit: `0x${string}`;
-    verificationGasLimit: `0x${string}`;
-    preVerificationGas: `0x${string}`;
-    maxFeePerGas: `0x${string}`;
-    maxPriorityFeePerGas: `0x${string}`;
-    paymaster?: `0x${string}`;
-    paymasterVerificationGasLimit?: `0x${string}`;
-    paymasterPostOpGasLimit?: `0x${string}`;
-    paymasterData?: `0x${string}`;
-    signature: `0x${string}`;
-}
+export type UserOpEncoded = UserOperation<"0.7", `0x${string}`>;
 
 /**
  * UserOp with decoded types
  * Bigint decoded from Hex stored on Firebase.
  */
-export interface UserOpDecoded {
-    sender: `0x${string}`;
-    nonce: bigint;
-    factory?: `0x${string}`;
-    factoryData?: `0x${string}`;
-    callData: `0x${string}`;
-    callGasLimit: bigint;
-    verificationGasLimit: bigint;
-    preVerificationGas: bigint;
-    maxFeePerGas: bigint;
-    maxPriorityFeePerGas: bigint;
-    paymaster?: `0x${string}`;
-    paymasterVerificationGasLimit?: bigint;
-    paymasterPostOpGasLimit?: bigint;
-    paymasterData?: `0x${string}`;
-    signature: `0x${string}`;
-}
+export type UserOpDecoded = UserOperation<"0.7", bigint>;
 
 /**
  * Zod validator encoding UserOpInput => UserOpEncoded
