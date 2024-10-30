3b1e4ea722967ee5d1e2a49eecad0964d84709aa
Leo Vigna <leo.vigna@gmail.com>
2024-10-30 16:49:22 +0700

    update notest

--- packages/contracts-create2factory/src/buildinfo.ts
+++ packages/contracts-create2factory/src/buildinfo.ts
@@ -106,7 +106,7 @@ export interface VerifyEtherscanParameters {
     contractAddress: Address;
     compilerVersion: string;
     sourceCode: string;
-    constructorArguments?: string;
+    constructorArguments?: Hex;
     evmVersion?: string;
 }
 export function verifyEtherscan({
@@ -169,8 +169,10 @@ export async function main() {
     const buildInfoPath = "artifacts/build-info/1afa25cf92de5ec814b300b7ef4c7ee1.json";
     const buildInfo: BuildInfo = JSON.parse(readFileSync(buildInfoPath, "utf-8"));
 
+    // file name key
     const name = "contracts/Create2Factory.sol";
     const contract = buildInfo.output.contracts[name];
+    // contract name (inside of file)
     const Create2Factory = contract.Create2Factory;
 
     //Looks like proper solc output???
@@ -186,8 +188,8 @@ export async function main() {
     //TODO: Already verified on Polygonscan, but try with different chain?
     const contractAddress: Address = "0x57318Dc30FE4da0a1b20eBbD4Dfd16aa66cfDB46";
 
+    // Delete extranuous "license" key disliked by Etherscan
     Object.values(metadata.sources).forEach((source: any) => {
-        // source.content = source.content.replaceAll(/"/g, "'");
         delete source.license;
     });
     //https://gist.github.com/0xV4L3NT1N3/974d6bfb58070e0fe4e38d626cdf1c44
@@ -241,10 +243,15 @@ export async function main() {
  *
  * - etherscanVerifySourceCode (rename & refactor)
  * - etherscanCheckVerifyStatus (rename & refactor)
- * - waitForEtherscanVerifyStatus - retry logic for when checkVerifyStatus returns "pending"
+ * - waitForEtherscanVerifyStatus - retry logic for when checkVerifyStatus returns "pending" (maybe check logic of viem waitForTransactionReceipt)
  *
  * - etherscanGetAbi (used to check if verified)
  * https://docs.polygonscan.com/api-endpoints/contracts#get-contract-abi-for-verified-contract-source-codes
+ *
+ * - Final higher level function that does the following steps
+ * 1. Check if contract at address is verified, if so return
+ * 2. Send verification request
+ * 3. Wait for verification request to succeed
  *
  */
 
