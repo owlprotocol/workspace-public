ec38dafa75eb9e8c92b7545446692bede8fc5432
tota79 <balashyabni@gmail.com>
2024-11-29 13:11:56 +0900

    create checkERC165InterfaceSupportList

--- packages/backend-indexer/src/actions/indexer/erc165.ts
+++ packages/backend-indexer/src/actions/indexer/erc165.ts
@@ -3,18 +3,30 @@ import { getChainId, readContract } from "viem/actions";
 import { erc165SupportsInterfaceResource } from "@owlprotocol/eth-firebase/admin";
 import { getAction } from "viem/utils";
 
+const defaultInterfaceIds: Hash[] = [
+    "0x01ffc9a7", // ERC165
+    "0x80ac58cd", // ERC721
+    "0x5b5e139f", // ERC721 Metadata
+    "0x780e9d63", // ERC721 Enumerable
+    "0xd9b67a26", // ERC1155
+    "0x36372b07", // ERC20
+];
+
 /**
  * Check if a contract supports an ERC165 interface.
  * @param client publicClient
- * @param address The contract address to check.
- * @param interfaceId The ERC165 interface ID.
+ * @param params An object containing the address and interfaceId to check.
  * @returns Whether the contract supports the given interface.
  */
 export async function checkERC165InterfaceSupport<chain extends Chain | undefined>(
     client: Client<Transport, chain>,
-    address: Address,
-    interfaceId: Hash,
-) {
+    params: {
+        address: Address;
+        interfaceId: Hash;
+    },
+): Promise<boolean> {
+    const { address, interfaceId } = params;
+
     const chainId = client.chain?.id ?? (await getAction(client, getChainId, "getChainId")({}));
     const cached = await erc165SupportsInterfaceResource.getOrNull({ chainId, address, interfaceId });
     if (cached) {
@@ -46,4 +58,38 @@ export async function checkERC165InterfaceSupport<chain extends Chain | undefine
     });
 
     return supported;
+}
+/**
+ * Check if a contract supports multiple ERC165 interfaces.
+ * @param client publicClient
+ * @param params An object containing the address and interfaceIds to check.
+ * @returns An array of supported interfaceIds.
+ */
+export async function checkERC165InterfaceSupportList<chain extends Chain | undefined>(
+    client: Client<Transport, chain>,
+    params: {
+        address: Address;
+        interfaceIds?: Hash[];
+    },
+): Promise<Hash[]> {
+    const { address, interfaceIds = defaultInterfaceIds } = params;
+
+    const supportedInterfaces: Hash[] = [];
+
+    for (const interfaceId of interfaceIds) {
+        try {
+            const supported = await checkERC165InterfaceSupport(client, { address, interfaceId });
+            if (supported) {
+                supportedInterfaces.push(interfaceId);
+            }
+        } catch (error) {
+            throw new Error(
+                `Error checking interface ${interfaceId} for contract ${address}: ${
+                    error instanceof Error ? error.message : "Unknown error"
+                }`,
+            );
+        }
+    }
+
+    return supportedInterfaces;
 }
