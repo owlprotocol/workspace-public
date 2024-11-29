56ad6e50306f8dee8cfcc3e9caf1b9ea684384d1
Oscar Baracos <oscar.baracos@gmail.com>
2024-11-29 19:32:02 +0100

    Add opbnb to supported networks

--- configs/envvars/src/genEnvVars.ts
+++ configs/envvars/src/genEnvVars.ts
@@ -226,7 +226,7 @@ export function getEnvVarsForNetworkId(networkId: string): EnvVarDef[] {
 //const chainIds = allChains.map((c) => c.chainId);
 const chainIds = [
     1, 5, 1337, 11155111, 17000, 11155420, 59144, 59140, 137, 80001, 80002, 42161, 421614, 10, 43114, 56, 97, 168587773,
-    8453, 84532, 3441006,
+    8453, 84532, 3441006, 204, 5611,
 ];
 //TODO: For all networkIds, right now this breaks because file is too big. Is there a better way?
 const NETWORK_ENVVARS: EnvVarDef[] = [];
