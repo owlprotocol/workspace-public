1e7e96dffe45bb45c00cf891bf8828d9be62f836
Oscar Baracos <oscar.baracos@gmail.com>
2024-12-02 17:53:36 +0100

    Add avalanche, avalanche fuji and kaia to chains

---
+++ packages/chains/src/chains/avalancheFuji.ts
@@ -0,0 +1,15 @@
+import { avalancheFuji as avalancheFujiViem } from "viem/chains";
+import { defineNetwork } from "../defineChain.js";
+
+export const avalancheFuji = /*#__PURE__*/ defineNetwork({
+    ...avalancheFujiViem,
+    testnet: true,
+    rpcUrls: {
+        ...avalancheFujiViem.rpcUrls,
+        public: avalancheFujiViem.rpcUrls.default,
+    },
+    slug: "avalanche-fuji",
+    slugAnkr: "avalanche-fuji",
+    slugDrpc: "avalanche-fuji",
+    enabled: true,
+});
