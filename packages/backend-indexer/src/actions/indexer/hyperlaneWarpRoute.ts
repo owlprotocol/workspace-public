50d36bc9c3ba68074b1ca8499b9e66b3ff633442
Kevin El Sayed <124922925+tota79@users.noreply.github.com>
2024-12-09 17:15:40 +0900

    Add function to retrieve collateral address from router address (#123)

--- packages/backend-indexer/src/actions/indexer/hyperlaneWarpRoute.ts
+++ packages/backend-indexer/src/actions/indexer/hyperlaneWarpRoute.ts
@@ -1,8 +1,9 @@
 import { Client, Transport, Address, Chain, Hex, zeroAddress, padHex } from "viem";
 import { readContract, getChainId } from "viem/actions";
-import { Router } from "@owlprotocol/contracts-hyperlane/artifacts";
+import { Router, HypERC20Collateral } from "@owlprotocol/contracts-hyperlane/artifacts";
 import { hyperlaneWarpRouteResource } from "@owlprotocol/eth-firebase/admin";
 import { getAction } from "viem/utils";
+import { HyperlaneWarpRouteData } from "@owlprotocol/eth-firebase/admin";
 
 /**
  * Fetch and upload all Hyperlane Warp Routes for a specific Router contract.
@@ -14,13 +15,15 @@ export async function getHyperlaneRoutes<chain extends Chain | undefined>(
     client: Client<Transport, chain>,
     params: { address: Address },
 ): Promise<{ domain: number; router: Hex }[]> {
-    const { address: tokenA } = params;
+    const { address } = params;
+
+    const tokenA = padHex(address, { size: 32 });
 
     const chainA = client.chain?.id ?? (await getAction(client, getChainId, "getChainId")({}));
 
     const domains: number[] = [
         ...(await readContract(client, {
-            address: tokenA,
+            address,
             abi: Router.abi,
             functionName: "domains",
         })),
@@ -28,7 +31,7 @@ export async function getHyperlaneRoutes<chain extends Chain | undefined>(
 
     const promises = domains.map((domain) =>
         readContract(client, {
-            address: tokenA,
+            address,
             abi: Router.abi,
             functionName: "routers",
             args: [domain],
@@ -44,26 +47,61 @@ export async function getHyperlaneRoutes<chain extends Chain | undefined>(
 
     const routes = results.filter((result): result is { domain: number; router: Hex } => result !== null);
 
-    const tokenRouters = routes.flatMap(({ domain, router }) => {
-        const tokenABytes32 = padHex(tokenA, { size: 32 });
+    const tokenRouters: HyperlaneWarpRouteData[] = routes.flatMap(({ domain, router }) => [
+        {
+            chainA,
+            tokenA,
+            chainB: domain,
+            tokenB: router,
+        },
+        {
+            chainA: domain,
+            tokenA: router,
+            chainB: chainA,
+            tokenB: tokenA,
+        },
+    ]);
 
-        return [
-            {
-                chainA,
-                tokenA: tokenABytes32,
-                chainB: domain,
-                tokenB: router,
-            },
-            {
-                chainA: domain,
-                tokenA: router,
-                chainB: chainA,
-                tokenB: tokenABytes32,
-            },
-        ];
-    });
+    const wrappedTokenRoute = await getTokenAddress(client, address, chainA);
+    if (wrappedTokenRoute) {
+        tokenRouters.push(wrappedTokenRoute);
+    }
 
     await hyperlaneWarpRouteResource.setBatch(tokenRouters);
 
     return routes;
+}
+
+/**
+ * Fetch and return collateral address.
+ * @param client publicClient
+ * @param address The router address.
+ * @param chainId The chain ID.
+ */
+async function getTokenAddress(
+    client: Client<Transport>,
+    address: Address,
+    chainId: number,
+): Promise<HyperlaneWarpRouteData | null> {
+    const tokenA = padHex(address, { size: 32 });
+    try {
+        // Fetch collateral address
+        const token = await readContract(client, {
+            address,
+            abi: HypERC20Collateral.abi,
+            functionName: "wrappedToken",
+        });
+
+        const tokenBytes32 = padHex(token as Address, { size: 32 });
+
+        return {
+            chainA: chainId,
+            tokenA: tokenBytes32, // collateral address
+            chainB: chainId,
+            tokenB: tokenA, // router address
+        };
+    } catch (error) {
+        console.warn(`Failed to fetch wrapped token for router ${address}:`, error);
+        return null;
+    }
 }
