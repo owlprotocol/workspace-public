4a589b95fa97d4206335cfa7708d5a00ed8efd2e
Oscar Baracos <oscar.baracos@gmail.com>
2024-12-13 11:21:36 +0100

    Verify ERC721 facets (#127)
    * Verify ERC721 facets
    
    * add all contracts to runVerifyAllContracts
    
    * Make verifier work with all contracts
    
    * Move some code to setupChainContracts
    
    ---------
    
    Co-authored-by: tota79 <balashyabni@gmail.com>

--- packages/chains-deploy/src/scripts/runVerifyAllContracts.ts
+++ packages/chains-deploy/src/scripts/runVerifyAllContracts.ts
@@ -1,37 +1,11 @@
 import { verifyContract } from "@owlprotocol/viem-utils";
 import { Address, encodeAbiParameters } from "viem";
-import { opBNBTestnet } from "@owlprotocol/chains";
-import { SolcMetadata } from "@owlprotocol/viem-utils";
+import { apeChain } from "@owlprotocol/chains";
 import { getMailboxAddressFromChainId } from "@owlprotocol/contracts-hyperlane";
-import * as HyperlaneMetadata from "@owlprotocol/contracts-hyperlane/solc-metadata";
-import * as ERC4337Metadata from "@owlprotocol/contracts-account-abstraction/solc-metadata";
-import * as DiamondMetadata from "@owlprotocol/contracts-diamond/solc-metadata";
-import * as Create2FactoryMetadata from "@owlprotocol/contracts-create2factory/solc-metadata";
 import { getChainEnvvars } from "@owlprotocol/envvars";
+import { ENTRYPOINT_ADDRESS_V07 } from "@owlprotocol/contracts-account-abstraction";
 import { getAllContractAddresses } from "../getAllContractAddresses.js";
-
-const allMetadata: Record<string, SolcMetadata> = {
-    ...HyperlaneMetadata,
-    ...ERC4337Metadata,
-    ...DiamondMetadata,
-    ...Create2FactoryMetadata,
-};
-
-const addressToMetadataMap: Record<string, string> = {
-    diamondCut: "DiamondCutFacet",
-    diamondLoupe: "DiamondLoupeFacet",
-    diamondInit: "DiamondInit",
-    diamondInitMulti: "DiamondInitMulti",
-    hypNative: "HypNative",
-};
-
-const mailboxToConstructorArgs = (mailboxAddress: Address) =>
-    encodeAbiParameters([{ name: "_mailbox", type: "address" }], [mailboxAddress]).slice(2);
-
-// TODO: Make this cleaner
-const addressToConstructorArgsMap = {
-    hypNative: mailboxToConstructorArgs,
-};
+import { addressToMailboxConstructorArgsMap, addressToMetadataMap, allMetadata } from "../setupChainContracts.js";
 
 async function verifyAllContracts(apiUrl: string, apiKey: string, mailboxAddress: Address | null) {
     const addresses = getAllContractAddresses({ mailboxAddress });
@@ -50,8 +24,36 @@ async function verifyAllContracts(apiUrl: string, apiKey: string, mailboxAddress
         }
 
         let constructorArguments;
-        if (contractAlias === "hypNative" && mailboxAddress) {
-            constructorArguments = addressToConstructorArgsMap.hypNative(mailboxAddress);
+        if (
+            mailboxAddress &&
+            (contractAlias === "hypNative" || contractAlias === "hypErc20" || contractAlias === "hypErc20Fast")
+        ) {
+            constructorArguments = addressToMailboxConstructorArgsMap[contractAlias](mailboxAddress);
+        } else if (contractAlias == "simpleAccountFactory") {
+            // NOTE: constructor args in etherscan don't accept the "0x" prefix
+            constructorArguments = encodeAbiParameters(
+                [{ name: "_entryPoint", type: "address" }],
+                [ENTRYPOINT_ADDRESS_V07],
+            ).slice(2);
+        } else if (contractAlias == "pimlicoEntrypointSimulations") {
+            constructorArguments = encodeAbiParameters(
+                [{ name: "_eps", type: "address" }],
+                [addresses["entrypointSimulations"]],
+            ).slice(2);
+        }
+
+        let settings;
+        if (contractAlias === "entrypointSimulations") {
+            // Taken from packages-public/packages/contracts-account-abstraction/hardhat.config.ts
+            settings = {
+                evmVersion: "paris",
+                viaIR: true,
+                optimizer: {
+                    enabled: true,
+                    runs: 10000,
+                    details: { yul: true },
+                },
+            };
         }
 
         await verifyContract({
@@ -60,6 +62,7 @@ async function verifyAllContracts(apiUrl: string, apiKey: string, mailboxAddress
             contractAddress: address,
             metadata: contractMetadata,
             constructorArguments,
+            settings,
         });
         console.log(`Verified ${contractAlias} at ${address}`);
     }
@@ -67,7 +70,7 @@ async function verifyAllContracts(apiUrl: string, apiKey: string, mailboxAddress
 
 // TODO: cleanup this script
 (async () => {
-    const network = opBNBTestnet;
+    const network = apeChain;
 
     const chainEnvVars = getChainEnvvars(network.chainId);
     const apiUrl = chainEnvVars.explorerApi;
