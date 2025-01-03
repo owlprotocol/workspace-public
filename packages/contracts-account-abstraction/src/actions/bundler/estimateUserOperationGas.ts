eeba4e2eca0ed17797982f0e2903dfa5bda9fc94
Leo Vigna <leo.vigna@gmail.com>
2025-01-03 19:26:08 +0100

    fix lint

--- packages/contracts-account-abstraction/src/actions/bundler/estimateUserOperationGas.ts
+++ packages/contracts-account-abstraction/src/actions/bundler/estimateUserOperationGas.ts
@@ -1,5 +1,4 @@
 import { Address, Chain, Client, Transport } from "viem";
-import * as chains from "viem/chains";
 import { EstimateUserOperationGasReturnType, UserOperation } from "viem/account-abstraction";
 import { getChainId } from "viem/actions";
 import { getAction } from "viem/utils";
