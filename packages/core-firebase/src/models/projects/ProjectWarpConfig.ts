f6d89fac8a1b619d842604ea514d5681ee0b7337
Kevin El Sayed <124922925+tota79@users.noreply.github.com>
2025-01-23 17:44:54 +0100

    add previous warp config id optional field (#149)
    * add previous warp config id optional field
    
    * add createdAt and logoURI fields
    
    * make createdAt optional

--- packages/core-firebase/src/models/projects/ProjectWarpConfig.ts
+++ packages/core-firebase/src/models/projects/ProjectWarpConfig.ts
@@ -26,6 +26,7 @@ export interface WarpTokenConfig {
     readonly symbol: string;
     readonly decimals: number;
     readonly connections: { token: string }[];
+    readonly logoURI?: string;
 }
 
 export interface ProjectWarpConfigData {
@@ -37,6 +38,8 @@ export interface ProjectWarpConfigData {
     readonly firstTokenAddress?: Address;
     readonly tokens: WarpTokenConfig[];
     readonly proxyDeploySalt?: Hex;
+    readonly createdAt?: number;
+    readonly previousWarpConfigId?: string;
 }
 
 const tokenConnectionZod = z.object({ token: z.string() });
@@ -49,6 +52,8 @@ export const projectWarpConfigDataZod = z
         deployerUser: z.string().optional(),
         firstTokenChainId: z.number().optional(),
         firstTokenAddress: addressZod.optional(),
+        previousWarpConfigId: z.string().optional(),
+        createdAt: z.number().int().positive().optional(),
         tokens: z.array(
             z.object({
                 standard: z.string(),
@@ -59,6 +64,7 @@ export const projectWarpConfigDataZod = z
                 symbol: z.string(),
                 decimals: z.number(),
                 connections: z.array(tokenConnectionZod),
+                logoURI: z.string().optional(),
             }),
         ),
         proxyDeploySalt: bytesZod.optional(),
