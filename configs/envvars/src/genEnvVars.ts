8d22be729e698102478edf3bc112d4df22da37f4
tota79 <balashyabni@gmail.com>
2025-02-03 17:21:59 +0100

    add verabridge emv

--- configs/envvars/src/genEnvVars.ts
+++ configs/envvars/src/genEnvVars.ts
@@ -276,6 +276,7 @@ export const ENVVARS: EnvVarDef[] = [
     { name: "OWL_DASHBOARD_URL", platform: "browser", defaultValue: "http://dashboard.owlprotocol.xyz" },
     { name: "OWL_AUTH_URL", platform: "browser", defaultValue: "https://auth.owl.build" },
     { name: "OWL_WALLET_URL", platform: "browser", defaultValue: "https://wallet.owl.build" },
+    { name: "VERABRIDGE_PROJECT_ID", platform: "browser", defaultValue: "d66a8de5-df4b-45fe-85cd-8576bfc8ea00" },
 ];
 
 const NODE_ENV_VAR = {
