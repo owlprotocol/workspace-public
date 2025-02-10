afc0ebe40cc072113eed7c04181ac7951430307b
Oscar Baracos <oscar.baracos@gmail.com>
2025-02-10 11:49:00 +0100

    Add authUserUID migration script

---
+++ packages/core-firebase/src/scripts/authUserUID.ts
@@ -0,0 +1,30 @@
+import { getFirebaseApp } from "@owlprotocol/crud-firebase/admin";
+import { userResource } from "../admin/resources.js";
+import { User } from "../models/User.js";
+
+export async function migrateAuthUserUID(email: string) {
+    const { auth } = getFirebaseApp();
+
+    const user = await auth.getUserByEmail(email);
+
+    const userId = user.uid;
+
+    const oldUserDocument = await userResource.getWhereFirst({ email });
+
+    if (!oldUserDocument) {
+        throw new Error("User document not found");
+    }
+
+    console.log({ authUserId: userId, oldUserId: oldUserDocument.userId });
+
+    const newUserDocument: User = { email, userId };
+
+    const userUpdates = [userResource.set(newUserDocument), userResource.delete(oldUserDocument)];
+
+    await Promise.all(userUpdates);
+}
+
+// if (!process.env.EMAIL) {
+//     throw new Error("EMAIL environment variable not set");
+// }
+// await migrateAuthUserUID(process.env.EMAIL!);
