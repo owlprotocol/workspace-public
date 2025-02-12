e234ecae8a0e6566527f4fa01e8bd157da203aae
tota79 <balashyabni@gmail.com>
2025-02-12 14:15:05 +0100

    add VERABRIDGE_ENABLED_CHAIN_IDS

--- configs/envvars/src/genEnvVars.ts
+++ configs/envvars/src/genEnvVars.ts
@@ -283,6 +283,7 @@ export const ENVVARS: EnvVarDef[] = [
     { name: "OWL_AUTH_URL", platform: "browser", defaultValue: "https://auth.owl.build" },
     { name: "OWL_WALLET_URL", platform: "browser", defaultValue: "https://wallet.owl.build" },
     { name: "VERABRIDGE_PROJECT_ID", platform: "browser", defaultValue: "d66a8de5-df4b-45fe-85cd-8576bfc8ea00" },
+    { name: "VERABRIDGE_ENABLED_CHAIN_IDS", platform: "browser" },
 ];
 
 const NODE_ENV_VAR = {
