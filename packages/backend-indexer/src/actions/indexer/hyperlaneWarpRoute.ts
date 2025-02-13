f6692c6aecb10835b3adb0681244b779a21341e1
Kevin El Sayed <124922925+tota79@users.noreply.github.com>
2025-02-13 10:42:26 +0100

    fix getHyperlaneRoutes (#169)
    * fix getHyperlaneRoutes
    
    * only store wrappedTokenAddress when tokenA is collateral
    
    * fix getHyperlaneRoutes
    
    * document getHyperlaneRoutes
    
    * fix type error

--- packages/backend-indexer/src/actions/indexer/hyperlaneWarpRoute.ts
+++ packages/backend-indexer/src/actions/indexer/hyperlaneWarpRoute.ts
@@ -6,20 +6,28 @@ import { getAction } from "viem/utils";
 import { HyperlaneWarpRouteData } from "@owlprotocol/eth-firebase/admin";
 
 /**
- * Fetch and upload all Hyperlane Warp Routes for a specific Router contract.
- * @param client publicClient
- * @param params Object containing the Router contract's address.
- * @returns A list of token routes as { domain: number; router: Hex }[].
+ * This function retrieves and uploads all Hyperlane Warp Routes for a given Router address.
+ * It assumes that the first chainId in the chainIds array, along with the router address, corresponds to the collateral token.
+ *
+ * @param client - The public client.
+ * @param params - Contains the router's address and a list of chain IDs.
+ * @param params.address - The router's address (preferably for the collateral token).
+ * @param params.chainIds - An array of chain IDs, with the first ID assumed to be the collateral token.
+ * @returns - List of token routes, each including a domain, router address, and optionally a wrapped token address.
  */
 export async function getHyperlaneRoutes<chain extends Chain | undefined>(
     client: Client<Transport, chain>,
-    params: { address: Address },
+    params: { address: Address; chainIds: number[] },
 ): Promise<{ domain: number; router: Hex; wrappedTokenAddress?: Address }[]> {
-    const { address } = params;
+    const { address, chainIds } = params;
+
+    const routes: { domain: number; router: Hex }[] = [];
 
     const tokenA = padHex(address, { size: 32 });
 
     const chainA = client.chain?.id ?? (await getAction(client, getChainId, "getChainId")({}));
+
+    routes.push({ domain: chainA, router: tokenA });
 
     let wrappedTokenAddress: Address | undefined;
     try {
@@ -32,48 +40,59 @@ export async function getHyperlaneRoutes<chain extends Chain | undefined>(
         console.warn(`Failed to fetch wrapped token for router ${address}:`, error);
     }
 
-    const domains: number[] = [
-        ...(await readContract(client, {
-            address,
-            abi: Router.abi,
-            functionName: "domains",
-        })),
-    ];
+    const domains = await readContract(client, {
+        address,
+        abi: Router.abi,
+        functionName: "domains",
+    });
 
-    const promises = domains.map((domain) =>
-        readContract(client, {
-            address,
-            abi: Router.abi,
-            functionName: "routers",
-            args: [domain],
-        })
-            .then((routerAddress: Hex) => (routerAddress !== zeroAddress ? { domain, router: routerAddress } : null))
-            .catch((error) => {
-                console.error(`Failed to fetch router for domain ${domain}:`, error);
-                return null;
-            }),
+    const invalidDomains = domains.filter((domain) => !chainIds.includes(domain, 1));
+    if (invalidDomains.length > 0) {
+        throw new Error(`Invalid domains found: ${invalidDomains.join(", ")}.`);
+    }
+
+    const routerResults = await Promise.all(
+        domains.map((domain) =>
+            readContract(client, {
+                address,
+                abi: Router.abi,
+                functionName: "routers",
+                args: [domain],
+            })
+                .then((routerAddress) => (routerAddress !== zeroAddress ? { domain, router: routerAddress } : null))
+                .catch((error) => {
+                    console.error(`Failed to fetch router for domain ${domain}:`, error);
+                    return null;
+                }),
+        ),
     );
 
-    const results = await Promise.all(promises);
+    const validRouterResults = routerResults.filter(
+        (result): result is { domain: number; router: Hex } => result !== null,
+    );
+    routes.push(...validRouterResults);
 
-    const routes = results.filter((result): result is { domain: number; router: Hex } => result !== null);
+    const tokenRouters: HyperlaneWarpRouteData[] = [];
+    for (let i = 0; i < routes.length; i++) {
+        for (let j = i + 1; j < routes.length; j++) {
+            const { domain: chainA, router: tokenA } = routes[i];
+            const { domain: chainB, router: tokenB } = routes[j];
 
-    const tokenRouters: HyperlaneWarpRouteData[] = routes.flatMap(({ domain, router }) => [
-        {
-            wrappedTokenAddress,
-            chainA,
-            tokenA,
-            chainB: domain,
-            tokenB: router,
-        },
-        {
-            wrappedTokenAddress,
-            chainA: domain,
-            tokenA: router,
-            chainB: chainA,
-            tokenB: tokenA,
-        },
-    ]);
+            tokenRouters.push({
+                wrappedTokenAddress: i === 0 ? wrappedTokenAddress : undefined,
+                chainA,
+                tokenA,
+                chainB,
+                tokenB,
+            });
+            tokenRouters.push({
+                chainA: chainB,
+                tokenA: tokenB,
+                chainB: chainA,
+                tokenB: tokenA,
+            });
+        }
+    }
 
     await hyperlaneWarpRouteResource.setBatch(tokenRouters);
 
